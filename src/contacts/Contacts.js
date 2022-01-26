083f84cbb2d12f0fd271d9830e0d8d45c330dd6b
Cate Uch <cat.uch@yandex.ru>
2022-01-27 02:12:23 +0300

    emailjs added

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -3,11 +3,15 @@ import Form from './form/Form';
 import Card from "../common/components/Card/Card";
 import Title from '../common/components/Title/Title';
 
+
 const Contacts = () => {
     return (
         <div className={style.contacts} id={'contacts'} >
        <Card className={style.contactsContainer}>
+
             <Title title={'Contact'}/>
+
+            <div className={style.map}><iframe scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=Moscow,%20+(My%20current%20location)&amp;t=k&amp;z=11&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" width="100%" height="300" frameborder="0"><a href="https://www.gps.ie/golf-gps/">golf range finders</a></iframe></div>
             <Form />
         </Card>
         </div>
--- src/contacts/form/Form.js
+++ src/contacts/form/Form.js
@@ -1,23 +1,53 @@
+import {useState, useEffect, useCallback} from 'react';
 import styles from "./Form.module.scss";
+import emailjs from 'emailjs-com';
 
 const Form = () => {
+
+  const [message, setMessage] = useState(null);
+
+
+const reset = () => {
+  setMessage(null)
+}
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('/service_gepsdmu', 'template_q7rr0cc', e.currentTarget, 'user_92ZidQ3LvL0Ct8GnQUBOq')
+      .then((result) => {
+          console.log(result.text);
+          setMessage('Successfuly sent');
+          setTimeout(reset, 3000)
+      }, (error) => {
+          console.log(error.text);
+          setMessage('Failed!');
+          setTimeout(reset, 3000)
+      });
+      e.currentTarget.reset();
+  };
+
   return (
-    <form className={styles.form} data-aos='fade-left'>
+    <form className={styles.form} onSubmit={sendEmail} data-aos='fade-left'>
         <div className={styles.inputBlock} >
       <ul >
         <li>
-          <input type="text" id="name" placeholder="Name" />
+          <input type="text" id="name" placeholder="Name" name='from_name' required/>
         </li>
         <li>
-          <input type="text" id="email" placeholder="Email" />
+          <input type="text" id="email" placeholder="Email" name='email' required/>
         </li>
         <li >
-        <textarea name="subject" placeholder="Message" />
+        <textarea name="subject" placeholder="Message" name='message' required/>
       </li>
       </ul>
 </div>
       <div className={styles.button} >
-        <button className={styles.ibButton}>Send Message</button>
+
+
+       {!message ? <button type={'submit'} className={styles.ibButton} >Send Message</button> : <p className={styles.text}>{message}</p> }
+
+
       </div>
     </form>
   );
